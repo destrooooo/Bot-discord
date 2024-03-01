4e7d0dcfdc46ab0d4a55dde83cc468e94463c8da
Neptwig <neptwig@gmail.com>
2024-03-01 17:26:16 +0100

    Feature: Search API

--- events/nico.js
+++ events/nico.js
@@ -3,13 +3,15 @@ const { Events } = require("discord.js");
 module.exports = {
   name: Events.MessageCreate,
   execute(message) {
-    const triggerWords = ["Nicolas", "Nico", "Triau"];
+    const triggerWords = ["Grégoire", "Greg", "gregoire", "grégoire", "greg"];
 
     if (message.author.bot) return false;
 
     triggerWords.forEach((word) => {
       if (message.content.includes(word)) {
-        message.reply("Est un déchet");
+        message.reply(
+          "https://cdn.discordapp.com/attachments/1211599095739842663/1211632876248891462/nique-gregoire.png?ex=65eee80c&is=65dc730c&hm=6197ea4db89019b8a582145c27c9c5fe3dff9aa58dc81f5e51bf7eb23d4c1acf&"
+        );
       }
     });
 
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 // Import required packages & token
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, GatewayIntentBits, Events } = require("discord.js");
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  ButtonStyle,
+} = require("discord.js");
 const { token } = require("./config.json");
+const { EmbedBuilder, ButtonBuilder } = require("@discordjs/builders");
 
 // Create a new client to run the bot
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 // Create a command collection
@@ -23,7 +29,7 @@ for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs
     .readdirSync(commandsPath)
-    .filter(file => file.endsWith(".js"));
+    .filter((file) => file.endsWith(".js"));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
@@ -41,7 +47,7 @@ for (const folder of commandFolders) {
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
   .readdirSync(eventsPath)
-  .filter(file => file.endsWith(".js"));
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
@@ -52,5 +58,75 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args));
   }
 }
+
+// Run the specified command
+client.on("interactionCreate", async (interaction) => {
+  try {
+    if (interaction.isCommand()) {
+      // Fetch the command in the Collection with that name and assign it to the variable command
+      const command = client.commands.get(interaction.commandName);
+      // If command does not exist, return
+      if (!command) return;
+      await command.execute(interaction);
+    }
+
+    if (interaction.isButton()) {
+      // Get the message that was interacted with
+      const message = interaction.message;
+      const messageID = message.id;
+
+      // Get the search result for that message from the resultMap
+      const searchResult = await resultMap.get(messageID);
+      if (interaction.customId === "prev") {
+        searchResult.prevSearch();
+      } else if (interaction.customId === "next") {
+        searchResult.nextSearch();
+      }
+
+      // Update the embed with information from new image
+      const oldEmbed = message.embeds[0];
+      const newEmbed = new EmbedBuilder(oldEmbed)
+        .setDescription(
+          `Result ${searchResult.currentResult + 1} of ${
+            searchResult.resultArray.length
+          }`
+        )
+        // .spliceFields(0, 1, {
+        //   name: searchResult.currentSearch().title,
+        //   value: searchResult.currentSearch().displayLink,
+        // })
+
+        .spliceFields(
+          0,
+          1,
+          { name: "Title", value: searchResult.currentSearch().title },
+          { name: "Link", value: searchResult.currentSearch().displayLink }
+        )
+
+        .setImage(await searchResult.currentSearch().link);
+
+      // Update the "View Original" button to point to the new image
+      const actionRow = message.components[0];
+      actionRow.spliceComponents(
+        2,
+        1,
+        new ButtonBuilder()
+          .setLabel("View Original")
+          .setStyle(ButtonStyle.Primary)
+          .setURL(searchResult.currentSearch().image.contextLink)
+      );
+
+      // Edit the original message
+      await interaction.update({ embeds: [newEmbed], components: [actionRow] });
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
 
 client.login(token);
